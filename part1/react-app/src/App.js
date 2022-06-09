47c2cd83d004e1c038fc3498b0c2659b773e8346
William Huang <wjhuang@bu.edu>
2022-06-09 17:31:37 -0400

    adjusted component in App.js

--- part1/react-app/src/App.js
+++ part1/react-app/src/App.js
@@ -1,7 +1,10 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const App = () => {
+  console.log('Hello from component')
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
 
 export default App

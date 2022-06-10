4168937c7e7567a4cd38b24ea911a411004acb7c
William Huang <wjhuang@bu.edu>
2022-06-09 20:53:23 -0400

    adjusted component again to see current time and to use constants

--- part1/react-app/src/App.js
+++ part1/react-app/src/App.js
@@ -1,8 +1,12 @@
 const App = () => {
-  console.log('Hello from component')
+  const now = new Date()
+  const a = 10
+  const b = 20
+
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>{a} plus {b} is {a + b}</p>
     </div>
   )
 }

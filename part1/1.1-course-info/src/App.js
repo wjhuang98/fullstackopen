1c10a58dbfaef81902632bc063a290aaf46ebdec
William Huang <wjhuang@bu.edu>
2022-06-11 15:35:18 -0400

    moved all components under App instead, need to fix content component

--- part1/1.1-course-info/src/App.js
+++ part1/1.1-course-info/src/App.js
@@ -1,33 +1,43 @@
-const Header = (header) => {
-  return (
-    <h1>{header.course}</h1>
-  )
-}
-
-const Content = (content) => {
-  return (
-    <div>
-    <p>part = content.parts[0].name exercises = content.parts[0].exercises</p>
-    <p>part = content.parts[1].name exercises = content.parts[1].exercises</p>
-    <p>part = content.parts[2].name exercises = content.parts[2].exercises</p>
-    </div>
-  )
-}
-
-const Total = (total) => {
-  return (
-    <p>Number of Exercises {total.parts[0].exercises + total.parts[1].exercises + total.parts[2].exercises}</p>
-  )
-}
-
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  const Header = (header) => {
+    return (
+      <h1>{header.course}</h1>
+    )
+  }
+  
+  const Content = (content) => {
+    return (
+      <div>
+      <p>part = content.parts[0].name exercises = content.parts[0].exercises</p>
+      <p>part = content.parts[1].name exercises = content.parts[1].exercises</p>
+      <p>part = content.parts[2].name exercises = content.parts[2].exercises</p>
+      </div>
+    )
+  }
+  
+  const Total = (total) => {
+    return (
+      <p>Number of Exercises {total.parts[0].exercises + total.parts[1].exercises + total.parts[2].exercises}</p>
+    )
+  }
 
   return (
     <div>
